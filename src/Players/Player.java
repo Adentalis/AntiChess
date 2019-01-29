8b8d877de33bdb799858ced2329b6e0f23017b4b
Daniel <daniel.n96@t-online.de>
2019-01-29 19:30:09 +0100

    added -attributes to Player

--- src/Players/Player.java
+++ src/Players/Player.java
@@ -1,13 +1,26 @@
 package Players;
 
+import Pieces.Piece;
 import helper.Colour;
+import logic.Cell;
+import logic.Move;
+
+import java.util.ArrayList;
 
 public abstract class Player {
     public String name;
     public Colour colour;
+    public ArrayList<Move> movesList;
+    public ArrayList<Piece> piecesList;
+    public Cell[][] map;
+
 
     Player(String name){
         this.name = name;
+    }
+
+    public  void createMovesList(){
+
     }
 
 
