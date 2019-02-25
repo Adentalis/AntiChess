8f857f1231cecc1b138a83fbf2b1cbfc608a9919
Daniel <daniel.n96@t-online.de>
2019-02-25 16:37:21 +0100

    refactor -Main

--- src/logic/AntiChess.java
+++ src/logic/AntiChess.java
@@ -11,15 +11,17 @@ public class AntiChess {
 
     public static void main(String[] var0) {
 
+        if(true) {
+            PvPGui board = new PvPGui();
+            board.setPlayerJLabels("Daniel", "Bot12");
+            board.setTurnLabel(board.getPlayer2Name());
+        }else{
+            RandomBot rndBot = new RandomBot("Bot1", Colour.WHITE);
+            RandomBot rndBot2 = new RandomBot("Bot2", Colour.BLACK);
+            Game game = new Game(rndBot,rndBot2);
+            BasicGui g = new BasicGui();
+        }
 
-        PvPGui board = new PvPGui();
-        board.setPlayerJLabels("Daniel", "Bot12");
-        board.setTurnLabel(board.getPlayer2Name());
-
-       // RandomBot rndBot = new RandomBot("Bot1", Colour.WHITE);
-        //RandomBot rndBot2 = new RandomBot("Bot2", Colour.BLACK);
-        //Game game = new Game(rndBot,rndBot2);
-        //BasicGui g = new BasicGui();
 
 
 
